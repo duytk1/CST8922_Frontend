798ab499572cc7ed341a4bfdaf53e187415a6fb0
bhun0002 <bhungaliyadeval@gmail.com>
2025-04-09 14:13:11 -0400

    Added Config File for Deployment

--- js/config.js
+++ js/config.js
@@ -1,1 +1,14 @@
-const API_BASE_URL = 'http://localhost:8080/api'; 
+window.API_BASE_URL = '';
+
+function loadConfig(callback) {
+  fetch('/config.json')
+    .then(response => response.json())
+    .then(config => {
+      window.API_BASE_URL = config.API_BASE_URL;
+      console.log("Loaded API_BASE_URL:", window.API_BASE_URL);
+      if (callback) callback();
+    })
+    .catch(error => {
+      console.error("Failed to load config.json", error);
+    });
+}
